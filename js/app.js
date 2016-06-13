513e294c1bc10e474a101366eeb1d9ed0fb3f8ac
Joe Seago <joe.seago@gmail.com>
2016-06-13 15:09:43 -0600

    added mouse enter and leave

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	/*console.log("hello world"); */
+
+$(".ryu").mouseenter(function() {
+	$(".ryustill").hide();
+	$(".ryuready").show();
+}).mouseleave(function() {
+	$(".ryuready").hide();
+	$(".ryustill").show();
+});
+
+
+
+
+
+
+});
