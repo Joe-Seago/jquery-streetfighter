5b4133edb09d13345f4ee9b36a938e34be2375c7
Joe Seago <joe.seago@gmail.com>
2016-06-20 04:22:50 -0600

    finish x keydown, clean up code

--- js/app.js
+++ js/app.js
@@ -1,28 +1,48 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
+  }).mouseleave(function() {
     $('.ryu-ready').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
     $('.ryu-still').show();
-  })
-  .mousedown(function() {
+  }).mousedown(function() {
   	playHadouken();
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
+    $('.ryu-still').hide();
     $('.ryu-throwing').show();
 	$('.hadouken').finish().show().animate( {'left': '1020px'}, 500, function() { 
 		$(this).hide();
 		$(this).css('left', '520px');
-	});
-
-  	}).mouseup(function() {
+		});
+	}).mouseup(function() {
     $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
+    $('.ryu-still').hide();
     $('.ryu-ready').show();
-  	});
+  });
+}).keydown(function(e) {
+	if (e.which === 88) {
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').hide();
+   		$('.ryu-cool').show();
+	};
+   }).keyup(function(e) {
+   	if (e.which === 88) {
+    	$('.ryu-throwing').hide();
+    	$('.ryu-ready').hide();
+    	$('.ryu-cool').hide();
+    	$('.ryu-still').show();
+    };
 });
+
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
-}
+};
